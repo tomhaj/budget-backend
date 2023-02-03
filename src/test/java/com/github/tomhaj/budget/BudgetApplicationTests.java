33360c38d2c17b8dace3a9ce3a369790612d6f07
Tomasz Hajduk <tmszhajduk@gmail.com>
2023-02-03 12:54:46 +0100

    Initial commit

---
+++ src/test/java/com/github/tomhaj/budget/BudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tomhaj.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
