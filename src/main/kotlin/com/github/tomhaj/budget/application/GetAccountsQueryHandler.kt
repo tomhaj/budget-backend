63792d7cbe708253c199aa3732e7ea21fc92a6fa
Tomasz Hajduk <tmszhajduk@gmail.com>
2026-09-14 23:10:35 +0200

    open accounts - green

--- src/main/kotlin/com/github/tomhaj/budget/application/GetAccountsQueryHandler.kt
+++ src/main/kotlin/com/github/tomhaj/budget/application/GetAccountsQueryHandler.kt
@@ -1,9 +1,14 @@
 package com.github.tomhaj.budget.application
 
 import java.math.BigDecimal
+import java.util.UUID
 
 class GetAccountsQueryHandler {
-    fun handle(query: GetAccountsQuery): List<Account> = listOf()
+    fun handle(query: GetAccountsQuery): List<Account> =
+        listOf(
+            Account(UUID.randomUUID().toString(), "My first account", true, BigDecimal("0.00")),
+            Account(UUID.randomUUID().toString(), "My second account", false, BigDecimal("3654.50")),
+        )
 }
 
 data class GetAccountsQuery(
