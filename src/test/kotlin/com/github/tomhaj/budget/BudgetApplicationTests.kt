18a01a4ae3704fd02df1fd1f0cd5d26640fc4957
Tomasz Hajduk <tmszhajduk@gmail.com>
2026-09-17 08:04:34 +0200

    jdbc, postgres, testcontainers

--- src/test/kotlin/com/github/tomhaj/budget/BudgetApplicationTests.kt
+++ src/test/kotlin/com/github/tomhaj/budget/BudgetApplicationTests.kt
@@ -1,10 +1,8 @@
 package com.github.tomhaj.budget
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class BudgetApplicationTests {
+class BudgetApplicationTests : IntegrationTest() {
     @Test
     fun contextLoads() {
     }
